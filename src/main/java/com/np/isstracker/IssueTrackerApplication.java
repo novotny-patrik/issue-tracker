8b8ca5d9604adef57cf3881dc9eccbbcd7baddc7
Patrik <novotny.patrik@post.cz>
2021-01-18 19:35:22 +0100

    Init

---
+++ src/main/java/com/np/isstracker/IssueTrackerApplication.java
@@ -0,0 +1,25 @@
+package com.np.isstracker;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+public class IssueTrackerApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(IssueTrackerApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(IssueTrackerApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner runner() {
+        return (args) -> log.info("Hello world!");
+    }
+
+}
